99ea1e6445fcee61274543142c4a4293d88400ca
nada <nona6066@gmail.com>
2018-02-14 12:52:54 +0200

    ready to master

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from blog import views
-from dajaxice.core import dajaxice_autodiscover, dajaxice_config
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 
